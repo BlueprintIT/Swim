8f75b8de55d7465d73d71b4f68baaa89cf88de9c
Dave Townsend <dtownsend@oxymoronical.com>
2005-06-15 13:40:33 +0000

    Version control now specifies the version allowing newer but not used versions.

--- includes/version.php
+++ includes/version.php
@@ -28,35 +28,40 @@ function getCurrentResource($dir)
 	}
 }
 
+function setCurrentVersion($dir,$version)
+{
+	$lock=fopen($dir.'/lock','a');
+	flock($lock,LOCK_EX);
+	
+	$vers=fopen($dir.'/version','w');
+	fwrite($vers,$version);
+	fclose($vers);
+	
+	flock($lock,LOCK_UN);
+	fclose($lock);
+}
+
 // Retrieves the latest version of a resource.
 function getCurrentVersion($dir)
 {
-	$newest=-1;
-	if ($res=@opendir($dir))
+	if (is_readable($dir.'/version'))
 	{
-		while (($file=readdir($res))!== false)
+		$lock=fopen($dir.'/lock','a');
+		flock($lock,LOCK_SH);
+		
+		$vers=fopen($dir.'/version','r');
+		$version=fgets($vers);
+		fclose($vers);
+		
+		flock($lock,LOCK_UN);
+		fclose($lock);
+	
+		if (is_dir($dir.'/'.$version))
 		{
-			if (!(substr($file,0,1)=='.'))
-			{
-				if ((is_dir($dir.'/'.$file))&&(is_numeric($file)))
-				{
-					if ($file>$newest)
-					{
-						$newest=$file;
-					}
-				}
-			}
+			return $version;
 		}
-		closedir($res);
 	}
-	if ($newest>=0)
-	{
-		return $newest;
-	}
-	else
-	{
-		return false;
-	}
+	return false;
 }
 
 // Retrieves a particular version of a resource.
