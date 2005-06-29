79cb3d5a873268917434399118ecff4976ff757f
Dave Townsend <dtownsend@oxymoronical.com>
2005-06-29 09:22:03 +0000

    Added host to the applet parameters

--- containers/internal/pages/htmledit/1/blocks/content/block.php
+++ containers/internal/pages/htmledit/1/blocks/content/block.php
@@ -19,7 +19,7 @@ $cancel->nested=&$request->nested;
 <div align="center">
 <applet class="com.blueprintit.webedit.WebEdit" width="90%" height="400"
  codebase="/internal/file/webedit" classpath="webedit.jar,log4j-1.2.9.jar,jdom.jar,swixml.jar,swim.jar">
- <param name="swim.base" value="<?= $prefs->getPref('url.pagegen') ?>">
+ <param name="swim.base" value="http://<?= $_SERVER['HTTP_HOST'] ?><?= $prefs->getPref('url.pagegen') ?>">
  <param name="style" value="/global/template/base/layout/content.css">
  <param name="html" value="<?= $request->resource.'/block.html' ?>">
 </applet>
--- containers/internal/pages/menuedit/1/blocks/content/block.php
+++ containers/internal/pages/menuedit/1/blocks/content/block.php
@@ -19,9 +19,9 @@ $cancel->nested=&$request->nested;
 <div align="center">
 <applet class="com.blueprintit.menuedit.MenuEdit" width="90%" height="400"
  codebase="/internal/file/webedit" classpath="menuedit.jar,log4j-1.2.9.jar,jdom.jar,swixml.jar,swim.jar">
- <param name="swim.base" value="<?= $prefs->getPref('url.pagegen') ?>">
+ <param name="swim.base" value="http://<?= $_SERVER['HTTP_HOST'] ?><?= $prefs->getPref('url.pagegen') ?>">
  <param name="menu" value="<?= $request->resource.'/block.xml' ?>">
- <param name="commit" value="<?= $commit->encode() ?>">
- <param name="cancel" value="<?= $cancel->encode() ?>">
+ <param name="commit" value="http://<?= $_SERVER['HTTP_HOST'] ?><?= $commit->encode() ?>">
+ <param name="cancel" value="http://<?= $_SERVER['HTTP_HOST'] ?><?= $cancel->encode() ?>">
 </applet>
 </div>
