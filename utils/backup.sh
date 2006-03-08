c39010426d33bd22337bbf8dbc2bed18b254ea7e
Dave Townsend <dtownsend@oxymoronical.com>
2006-03-08 10:17:42 +0000

    Updates for design container.

--- utils/backup.sh
+++ utils/backup.sh
@@ -15,8 +15,7 @@ TARGET=$BASE.tar.gz
 rm -f $TARGET
 rm -f $TEMP_FILE
 
-find site/content \( -name temp -o -name .svn -o -name dir.lock \) -prune -o -print | tar -cf $TEMP_FILE --exclude=.htaccess --no-recursion -T -
+find site \( -name temp -o -name .svn -o -name dir.lock -o -name addons \) -prune -o -print | tar -cf $TEMP_FILE --exclude=.htaccess --no-recursion -T -
 tar -rf $TEMP_FILE --ignore-failed-read bootstrap/host.conf >/dev/null 2>/dev/null
-tar -rf $TEMP_FILE --ignore-failed-read --exclude=.htaccess site/config
 
 gzip $TEMP_FILE
