20d9a4b2caed978aa4d8fa6599adb9358bb72ad3
Dave Townsend <dtownsend@oxymoronical.com>
2006-03-09 15:00:52 +0000

    Use new treeview in page selector.

--- swim/internal/pages/pageselect/blocks/leftpane/block.php
+++ swim/internal/pages/pageselect/blocks/leftpane/block.php
@@ -1,25 +1,35 @@
+<stylesheet src="/internal/file/yahoo/css/folders/tree.css"/>
+<script src="/internal/file/yahoo/YAHOO.js"/>
+<script src="/internal/file/yahoo/event.js"/>
+<script src="/internal/file/yahoo/treeview.js"/>
+<script src="/internal/file/scripts/treeview.js"/>
 <?
 
-class LinkedCategoryTree extends PageTree
+class LinkedCategoryTree extends YahooPageTree
 {
-  function LinkedCategoryTree($root)
+  function LinkedCategoryTree($id, $root)
   {
-    $this->PageTree($root);
+    $this->YahooPageTree($id, $root);
   }
   
-  function displayPageLabel($page)
+  function getItemLink($page)
   {
-    global $request,$pages;
+    global $request;
     
-    print('<span>');
-    $link = new Request();
-    $link->resource = $request->resource;
-    $link->method=$request->method;
-    $link->query['page']=$page->getPath();
-    print('<a href="'.$link->encode().'">');
-    parent::displayPageLabel($page);
-    print('</a>');
-    print('</span>');
+    if ($page instanceof Page)
+    {
+      $request = SwimEngine::getCurrentRequest();
+      
+      if ($request->resource!=$page->getPath())
+      {
+        $link = new Request();
+        $link->resource = $request->resource;
+        $link->method=$request->method;
+        $link->query['page']=$page->getPath();
+        return $link->encode();
+      }
+    }
+    return parent::getItemLink($page);
   }
 }
 
@@ -28,14 +38,13 @@ class LinkedCategoryTree extends PageTree
 <h2>Structure</h2>
 </div>
 <div class="body">
-<ul class="categorytree">
+<div id="categorytree"></div>
 <?
 $cont = $_PREFS->getPref('container.default');
 if (isset($request->query['container']))
   $cont = $request->query['container'];
 $cm = getContainer($cont);
-$tree = new LinkedCategoryTree($cm->getRootCategory());
+$tree = new LinkedCategoryTree("categorytree", $cm->getRootCategory());
 $tree->display();
 ?>
-</ul>
 </div>
