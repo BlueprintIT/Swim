80c42eeddef7f4fda37e45a880509864515faa03
Dave Townsend <dtownsend@oxymoronical.com>
2006-07-26 13:54:31 +0000

    Sitemaps must appear at the root of the site.

---
+++ swim/methods/sitemap.php
@@ -0,0 +1,37 @@
+<?
+
+/*
+ * Swim
+ *
+ * Google Sitemap method
+ *
+ * Copyright Blueprint IT Ltd. 2006
+ *
+ * $HeadURL$
+ * $LastChangedBy$
+ * $Date$
+ * $Revision$
+ */
+
+function method_sitemap($request)
+{
+  global $_USER, $_STORAGE, $_PREFS;
+  
+  $log = LoggerManager::getLogger('swim.method.admin');
+  checkSecurity($request, false, true);
+  
+  $path = $_PREFS->getPref('storage.admin.templates').'/sitemap.tpl.xml';
+  if ($path != null)
+  {
+    $type = determineContentType($path);
+    setContentType($type);
+    $smarty = createAdminSmarty($request, $type);
+    $log->debug('Starting display.');
+    $smarty->display($path);
+    $log->debug('Display complete.');
+  }
+  else
+    displayNotFound($request);
+}
+
+?>
