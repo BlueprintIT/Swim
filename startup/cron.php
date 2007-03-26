6b928912f62565bc66506f8a859d54d774f522fe
Dave Townsend <dtownsend@oxymoronical.com>
2007-03-26 15:42:03 +0000

    Display new logs.

--- startup/cron.php
+++ startup/cron.php
@@ -67,6 +67,8 @@ $mainhost = $_PREFS->getPref('url.host.1.hostname');
 LoggerManager::setLogOutput('',new StdOutLogOutput($mainhost.' [$[txtlevel+5]] $[logger+30]: $[text] ($[file]:$[line])', $mainhost.'       $[logger+30]: $[function]$[arglist] ($[file]:$[line])'));
 $log = Loggermanager::getLogger('swim.cron');
 
+LoggerManager::setLogLevel('swim.cron',LOG_LEVEL_INFO);
+
 $log->info("Cron job startup");
 
 SwimEngine::ensureStarted();
